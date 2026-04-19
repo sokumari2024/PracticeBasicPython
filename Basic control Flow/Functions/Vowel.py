69c64b7a1fe997317816414eaa353633c8f3b80d
sokumari <sonu210.1570.sk24@gmail.com>
2026-04-19 14:34:24 +0530

    Vowels

---
+++ Basic control Flow/Functions/Vowel.py
@@ -0,0 +1,10 @@
+def is_vowel(char):
+    if char in ['a','i','o','u','e','A','I','O','U','E']:
+        print(A,"is a vowel")
+        return A
+    else:
+        print(A,"is not a vowel")
+        return A
+
+A=input("Enter a character:")
+is_vowel(A)

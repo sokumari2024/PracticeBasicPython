4a04541b3a70a1cb370c8bdad16fbcacf4473e5e
sokumari <sonu210.1570.sk24@gmail.com>
2026-04-20 23:19:29 +0530

    updated script

---
+++ Basic control Flow/Functions/AdditionalquestionQ1.py
@@ -0,0 +1,12 @@
+n=int(input("Enter a number: "))
+def sequence(n):
+    i=1
+    for i in range(i,n+1):
+        print(i,end=" ")
+    print("\n") 
+    for i in range(n,0,-1):
+        print(i,end=" ")
+sequence(n)
+
+
+
